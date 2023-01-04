859b7378cc85de5299d1ff8dfc4267e02845266e
Lucas (Boxuga) <86517395+BoxxyDEV@users.noreply.github.com>
2023-01-04 16:52:28 +0800

    made new to get more images

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import webbrowser
 #prints a title
 print("Find working prnt.sc links!")
 
-abc = ["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+abc = ["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 # just lists with charcters prob a more efficiant way to do it but yeah
 countermax = int(input('How links would you like to generate? ')) # asks user how many it should have
 openinbrowser = int(input('Would you like to open working links in default browser? [1. Yes/2. No] ')) # if user wants to open in a browser
@@ -16,12 +15,12 @@ counter = 0 # Counts succussful links
 fail = 0 # counts connection errors
 tries = 0 # counts how many tries it has done
 while countermax > counter:
-    url = "https://prnt.sc/" + abc[random.randint(0, len(abc)-1)] + abc[random.randint(0, len(abc)-1)] + num[random.randint(0, len(num)-1)] + num[random.randint(0, len(num)-1)] + num[random.randint(0, len(num)-1)]
+    url = "https://prnt.sc/" + abc[random.randint(0, len(abc)-1)] + abc[random.randint(0, len(abc)-1)] + abc[random.randint(0, len(abc)-1)] + abc[random.randint(0, len(abc)-1)] + abc[random.randint(0, len(abc)-1)]
     #generates urls
     try:
         req = requests.get(url, headers={'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0"}) 
         # Custom User Agent specified because cloudflare/lightshot blocks python user agent for Firefox 108.0 on Linux x64 but this can be changed to whatever
-        if req.text.find("i.imgur.com") >= 0:
+        if req.text.find("i.imgur.com") >= 0 or req.text.find("https://image.prntscr.com/image/") >= 0:
             print(url)
             if openinbrowser == 1: #opens url if user wants this option
                 webbrowser.open(url)
